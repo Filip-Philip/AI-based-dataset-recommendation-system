25aea5f76b934ea5ab59f5117a6c5e772f7429c1
DESKTOP-3759NNU\Filip <skalka.fil@gmail.com>
2023-04-27 21:21:53 +0200

    zenodo_files

--- DataCollectorClient/src/services/ParserBase.py
+++ DataCollectorClient/src/services/ParserBase.py
@@ -3,6 +3,30 @@ import pickle
 # from dataclasses.DataCluster import DataCluster
 # from dataclasses.EmbeddingBase import EmbeddingBase
 from pandas import DataFrame
+from typing import Dict, Set
+import numpy as np
+
+STD_SIZE = "std_size"
+MEAN_SIZE = "mean_size"
+COUNT = "count"
+SIZES = "sizes"
+PATHS = "paths"
+
+
+def update_files_data(files_data: Dict, filetypes: Set) -> Dict:
+    for filetype in filetypes:
+        sizes = files_data.pop(f"{filetype}_{SIZES}")
+        mean_size = sum(sizes) / files_data[f"{filetype}_{COUNT}"]
+        files_data[f"{filetype}_{MEAN_SIZE}"] = mean_size
+        std_size = np.sqrt(sum((np.array(sizes) - np.ones(len(sizes)) * mean_size) ** 2) / len(sizes))
+        files_data[f"{filetype}_{STD_SIZE}"] = std_size
+
+    return files_data
+
+
+def save_json(repository: str, json_object: str, from_date: str, to_date: str):
+    with open(f"../../../data/{repository}/backup_jsons/{from_date}-{to_date}.json", "w") as outfile:
+        outfile.write(json_object)
 
 
 class ParserBase(ABC):
